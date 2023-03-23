c94e18f984497ff2c24447083d38fe3690554370
liujia <liujia12101@163.com>
2023-03-23 18:37:42 +0800

    chore:配置

---
+++ .prettierrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  // 在语句末尾打印分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 需要时在对象属性上添加引号
+  quoteProps: 'as-needed',
+  // JSX中使用单引号
+  jsxSingleQuote: true,
+  // 尾部逗号
+  trailingComma: 'none',
+  // 大括号内左右两侧空格
+  bracketSpacing: true,
+  // 已弃用
+  // jsxBracketSameLine: false,
+  // 将多行HTML元素的 > 单独放在下一行
+  bracketSameLine: false,
+  // 箭头函数单个参数省略括号
+  arrowParens: 'always',
+  // 是否在文件顶部插入一个特殊的@format标记
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'ignore',
+  // vue script和style标签中是否缩进
+  vueIndentScriptAndStyle: false,
+  // 换行
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto'
+}
